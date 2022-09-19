81f5b5e91da22431992e5a17222fa06bb7fb7667
Philip <62082747+philip-marshall@users.noreply.github.com>
2022-09-19 13:55:07 -0600

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -1,14 +1,18 @@
-const http = require('http');
-
-const hostname = '69.48.142.114';
-const port = 80;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// Requiring module
+const express = require('express');
+ 
+// Creating express object
+const app = express();
+ 
+// Defining port number
+const PORT = 80;                 
+ 
+// Function to serve all static files
+// inside public directory.
+app.use(express.static('public')); 
+app.use('/images', express.static('images'));
+ 
+// Server setup
+app.listen(PORT, () => {
+  console.log(`Running server on PORT ${PORT}...`);
+})
